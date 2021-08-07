58ad2c4c17c695202751750c0f56e6baba46dadd
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2021-08-07 15:24:50 -0700

    header formatting

--- src/components/siteLayout.js
+++ src/components/siteLayout.js
@@ -2,19 +2,16 @@ import React from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
 import "../styles/index.scss"
-import * as layoutStyles from  "./layout.module.scss"
 import * as headerStyles from './header.module.scss'
 
 import { StickyContainer, Sticky } from 'react-sticky';
 
 const SiteLayout = (props) => {
   return (
-    <div className={layoutStyles.container}>
+    <div className={headerStyles.container}>
     <StickyContainer>
       <Sticky>{({ style }) => <header className={headerStyles.header} style={style}><Header /></header>}</Sticky>
-        <div className={layoutStyles.content}>
-          {props.children}
-        </div>
+      {props.children}
       <Footer />
     </StickyContainer>
     </div>
